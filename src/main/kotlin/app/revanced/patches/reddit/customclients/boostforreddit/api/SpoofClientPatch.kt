89220b92d8b74c78f089a74e4c904606817e59b4
Alex <alexkas@gmail.com>
2024-05-30 17:23:41 -0400

    useragent thing

--- src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/api/SpoofClientPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/customclients/boostforreddit/api/SpoofClientPatch.kt
@@ -7,7 +7,6 @@ import app.revanced.patches.reddit.customclients.BaseSpoofClientPatch
 import app.revanced.patches.reddit.customclients.boostforreddit.api.fingerprints.GetClientIdFingerprint
 import app.revanced.patches.reddit.customclients.boostforreddit.api.fingerprints.LoginActivityOnCreateFingerprint
 
-
 @Suppress("unused")
 object SpoofClientPatch : BaseSpoofClientPatch(
     redirectUri = "http://rubenmayayo.com",
@@ -19,8 +18,19 @@ object SpoofClientPatch : BaseSpoofClientPatch(
         first().mutableMethod.addInstructions(
             0,
             """
-                 const-string v0, "$clientId"
-                 return-object v0
+                const-string v0, "$clientId"
+                return-object v0
+            """
+        )
+    }
+
+    override fun Set<MethodFingerprintResult>.patchUserAgent(context: BytecodeContext) {
+        first().mutableMethod.addInstructions(
+            0,  // Adjust this index according to the correct line in the targeted method
+            """
+                const-string v0, "com.rubenmayayo.reddit:1.12.12-210011212"
+                invoke-virtual {p0, v0}, Landroid/webkit/WebSettings;->setUserAgentString(Ljava/lang/String;)V
+                return-object v0
             """
         )
     }
